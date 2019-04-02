584210a1824b7b46836836fcb1a5744f10a052fd
Jonathan Reinink <jonathan@reinink.ca>
2019-04-02 13:11:24 -0400

    Add welcome message to dashboard

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <layout title="Dashboard">
     <h1 class="mb-8 font-bold text-3xl">Dashboard</h1>
-    <div class="mt-16">
+    <p class="mb-12 leading-normal">Hey there! Welcome to Ping CRM, a demo app designed to help illustrate how <a class="text-indigo underline hover:text-orange-dark" href="https://github.com/inertiajs">Inertia.js</a> works.</p>
+    <div>
       <inertia-link class="btn-indigo" href="/500">500 error</inertia-link>
       <inertia-link class="btn-indigo" href="/404">404 error</inertia-link>
     </div>
