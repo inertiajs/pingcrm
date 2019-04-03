b5f919e88c5a0249daa3a356b8576a34698cebc1
Jonathan Reinink <jonathan@reinink.ca>
2019-04-03 08:08:38 -0400

    Switch from block to inline

--- resources/js/Shared/SearchFilter.vue
+++ resources/js/Shared/SearchFilter.vue
@@ -3,7 +3,7 @@
     <div class="flex w-full bg-white shadow rounded">
       <dropdown class="px-4 md:px-6 rounded-l border-r hover:bg-grey-lightest focus:border-white focus:shadow-outline focus:z-10" placement="bottom-start">
         <div class="flex items-baseline">
-          <span class="text-grey-darkest hidden md:block">Filter</span>
+          <span class="text-grey-darkest hidden md:inline">Filter</span>
           <svg class="w-2 h-2 fill-grey-darker md:ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 961.243 599.998">
             <path d="M239.998 239.999L0 0h961.243L721.246 240c-131.999 132-240.28 240-240.624 239.999-.345-.001-108.625-108.001-240.624-240z" />
           </svg>
