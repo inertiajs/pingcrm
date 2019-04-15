aa2bf5202cce89bb31b856b3008466fa582998fb
Jonathan Reinink <jonathan@reinink.ca>
2019-04-15 19:05:48 -0400

    Make pagination links update history

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -2,7 +2,7 @@
   <div class="mt-6 -mb-1 flex flex-wrap">
     <template v-for="(link, key) in links">
       <div v-if="link.url === null" :key="key" class="mr-1 mb-1 px-4 py-3 text-sm border rounded text-grey" :class="{ 'ml-auto': link.label === 'Next' }">{{ link.label }}</div>
-      <inertia-link v-else :key="key" replace class="mr-1 mb-1 px-4 py-3 text-sm border rounded hover:bg-white focus:border-indigo focus:text-indigo" :class="{ 'bg-white': link.active, 'ml-auto': link.label === 'Next' }" :href="link.url">{{ link.label }}</inertia-link>
+      <inertia-link v-else :key="key" class="mr-1 mb-1 px-4 py-3 text-sm border rounded hover:bg-white focus:border-indigo focus:text-indigo" :class="{ 'bg-white': link.active, 'ml-auto': link.label === 'Next' }" :href="link.url">{{ link.label }}</inertia-link>
     </template>
   </div>
 </template>
