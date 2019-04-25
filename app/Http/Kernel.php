0e94b372d14f505f38d389046694e3c19e6fdade
Jonathan Reinink <jonathan@reinink.ca>
2019-04-25 11:18:18 -0400

    Update to new Inertia middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\ConvertRedirects::class,
     ];
 
     /**
@@ -29,7 +28,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \Inertia\CheckInertiaVersion::class,
+            \Inertia\Middleware::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
