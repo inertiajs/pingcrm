8f2a6f57e09482ad3ae29a5fb928806a2980da75
Jonathan Reinink <jonathan@reinink.ca>
2019-05-22 11:53:51 -0400

    Improve flash messages

--- resources/js/Shared/TrashedMessage.vue
+++ resources/js/Shared/TrashedMessage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="p-4 bg-yellow-400 rounded flex items-center justify-between max-w-3xl">
     <div class="flex items-center">
-      <icon name="trash" class="flex-shrink-0 w-4 h-4 fill-yellow-900 mr-2" />
-      <div class="text-yellow-900">
+      <icon name="trash" class="flex-shrink-0 w-4 h-4 fill-yellow-800 mr-2" />
+      <div class="text-sm font-medium text-yellow-800">
         <slot />
       </div>
     </div>
-    <button class="text-yellow-900 hover:underline" tabindex="-1" type="button" @click="$emit('restore')">Restore</button>
+    <button class="text-sm text-yellow-800 hover:underline" tabindex="-1" type="button" @click="$emit('restore')">Restore</button>
   </div>
 </template>
 
