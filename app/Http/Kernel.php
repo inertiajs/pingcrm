92321aeb1e099845609ddd248e4abc9ce1d56a8f
Jonathan Reinink <jonathan@reinink.ca>
2019-08-12 08:37:51 -0400

    Make Inertia middleware last

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,7 +28,6 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \Inertia\Middleware::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -36,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Inertia\Middleware::class,
         ],
 
         'api' => [
