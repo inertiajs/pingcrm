14352710ad577e45062a49d19a6a6d441e958fb6
Jonathan Reinink <jonathan@reinink.ca>
2019-08-13 13:36:25 -0400

    Update to tagged Inertia dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,16 @@
-import Inertia from 'inertia-vue'
+import { InertiaApp } from '@inertiajs/inertia-vue'
 import PortalVue from 'portal-vue'
 import Vue from 'vue'
 
 Vue.config.productionTip = false
 Vue.mixin({ methods: { route: window.route } })
-Vue.use(Inertia)
+Vue.use(InertiaApp)
 Vue.use(PortalVue)
 
 let app = document.getElementById('app')
 
 new Vue({
-  render: h => h(Inertia, {
+  render: h => h(InertiaApp, {
     props: {
       initialPage: JSON.parse(app.dataset.page),
       resolveComponent: name => import(`@/Pages/${name}`).then(module => module.default),
