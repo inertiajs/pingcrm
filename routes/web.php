57e7d3ba0a3f5c0c66c1c2041d2182beaa069566
Jonathan Reinink <jonathan@reinink.ca>
2019-12-18 17:13:57 -0500

    Force debug mode for this endpoint in the demo environment
    Thanks to @freekmurze for confirming this is safe, and won't expose any environment variables

--- routes/web.php
+++ routes/web.php
@@ -54,5 +54,10 @@ Route::get('reports')->name('reports')->uses('ReportsController')->middleware('a
 
 // 500 error
 Route::get('500', function () {
+    // Force debug mode for this endpoint in the demo environment
+    if (App::environment('demo')) {
+        Config::set('app.debug', true);
+    }
+
     echo $fail;
 });
