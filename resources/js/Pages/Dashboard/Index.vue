702f76a024a54daac6eb2d3ff1e22b2a7df52a43
Jonathan Reinink <jonathan@reinink.ca>
2019-12-18 15:15:06 -0500

    Update Inertia.js website link

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="mb-8 font-bold text-3xl">Dashboard</h1>
-    <p class="mb-12 leading-normal">Hey there! Welcome to Ping CRM, a demo app designed to help illustrate how <a class="text-indigo underline hover:text-orange-dark" href="https://github.com/inertiajs">Inertia.js</a> works.</p>
+    <p class="mb-12 leading-normal">Hey there! Welcome to Ping CRM, a demo app designed to help illustrate how <a class="text-indigo underline hover:text-orange-dark" href="https://inertiajs.com">Inertia.js</a> works. </p>
     <div>
       <inertia-link class="btn-indigo" href="/500">500 error</inertia-link>
       <inertia-link class="btn-indigo" href="/404">404 error</inertia-link>
