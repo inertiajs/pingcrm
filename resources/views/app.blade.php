a8e0d42aec397cf730f04af15cee4a47845d1f87
Jonathan Reinink <jonathan@reinink.ca>
2020-02-14 14:29:07 -0500

    Add IE11/Edge polyfills

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+
+    {{-- Inertia --}}
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=smoothscroll,NodeList.prototype.forEach,Promise,Object.values,Object.assign" defer></script>
+
+    {{-- Ping CRM --}}
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith" defer></script>
+
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
 </head>
