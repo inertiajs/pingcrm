f6093a4df065041d2d09f08ab41f08bb0726056d
alessandro <alessandro.fuda@gmail.com>
2020-09-25 15:51:42 +0200

    Update Login.vue
    Into Login form, password input: onclick on svg icon --> toggle text/password input type.

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -7,7 +7,11 @@
           <h1 class="text-center font-bold text-3xl">Welcome Back!</h1>
           <div class="mx-auto mt-6 w-24 border-b-2" />
           <text-input v-model="form.email" :error="errors.email" class="mt-10" label="Email" type="email" autofocus autocapitalize="off" />
-          <text-input v-model="form.password" class="mt-6" label="Password" type="password" />
+          <text-input v-model="form.password" class="mt-6 inline-block w-full" label="Password" :type="type" />
+          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye inline-block relative cursor-pointer -ml-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" @click="changeType">
+            <path fill-rule="evenodd" d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 0 0 1.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0 0 14.828 8a13.133 13.133 0 0 0-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 0 0 1.172 8z" />
+            <path fill-rule="evenodd" d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+          </svg>
           <label class="mt-6 select-none flex items-center" for="remember">
             <input id="remember" v-model="form.remember" class="mr-1" type="checkbox">
             <span class="text-sm">Remember Me</span>
@@ -45,6 +49,7 @@ export default {
         password: 'secret',
         remember: null,
       },
+      type: 'password',
     }
   },
   methods: {
@@ -59,6 +64,9 @@ export default {
         onStart: () => this.sending = true,
         onFinish: () => this.sending = false,
       })
+    },
+    changeType() {
+      this.type = this.type === 'password' ? this.type = 'text' : this.type = 'password'
     },
   },
 }
