60654503974293111d0fe3fa533b5f0eca9cde68
Jonathan Reinink <jonathan@reinink.ca>
2020-10-08 10:04:20 -0400

    Use new terse named exports

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,27 @@
 import Vue from 'vue'
 import VueMeta from 'vue-meta'
 import PortalVue from 'portal-vue'
-import { InertiaApp } from '@inertiajs/inertia-vue'
-import { InertiaProgress } from '@inertiajs/progress/src'
+import { app, plugin } from '@inertiajs/inertia-vue'
+import { InertiaProgress as progress } from '@inertiajs/progress/src'
 
 Vue.config.productionTip = false
 Vue.mixin({ methods: { route: window.route } })
-Vue.use(InertiaApp)
+Vue.use(plugin)
 Vue.use(PortalVue)
 Vue.use(VueMeta)
 
-InertiaProgress.init()
+progress.init()
 
-let app = document.getElementById('app')
+let el = document.getElementById('app')
 
 new Vue({
   metaInfo: {
     titleTemplate: (title) => title ? `${title} - Ping CRM` : 'Ping CRM'
   },
-  render: h => h(InertiaApp, {
+  render: h => h(app, {
     props: {
-      initialPage: JSON.parse(app.dataset.page),
+      initialPage: JSON.parse(el.dataset.page),
       resolveComponent: name => import(`@/Pages/${name}`).then(module => module.default),
     },
   }),
-}).$mount(app)
+}).$mount(el)
