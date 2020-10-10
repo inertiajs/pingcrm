435c6a49e4136201ee7c5e27ceaa849314696616
Claudio Dekker <claudiodekker@users.noreply.github.com>
2020-10-10 12:51:07 +0200

    Update Middleware

--- app/Http/Middleware/Inertia.php
+++ app/Http/Middleware/Inertia.php
@@ -2,63 +2,52 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
-use Inertia\Concerns\InertiaDefaults;
+use Illuminate\Http\Request;
 use Inertia\Middleware;
 
 class Inertia extends Middleware
 {
-    use InertiaDefaults {
-        share as defaultShare;
-        version as defaultVersion;
-    }
-
     /**
-     * Determine the current Inertia asset version hash
-     * used for automatic asset cache busting.
+     * Determines the current Inertia asset version hash.
      *
      * @see https://inertiajs.com/asset-versioning
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    public function version($request)
+    public function version(Request $request)
     {
         return md5_file(public_path('mix-manifest.json'));
     }
 
     /**
-     * Defines the Inertia properties that automatically
-     * shared as of default. Can be overwritten.
+     * Defines the Inertia props that are shared by default.
      *
      * @see https://inertiajs.com/shared-data
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
-    public function share($request)
+    public function share(Request $request)
     {
-        return array_merge($this->defaultShare($request), [
-            'auth' => function () {
+        return array_merge(parent::share($request), [
+            'auth' => function () use ($request) {
                 return [
-                    'user' => Auth::user() ? [
-                        'id' => Auth::user()->id,
-                        'first_name' => Auth::user()->first_name,
-                        'last_name' => Auth::user()->last_name,
-                        'email' => Auth::user()->email,
-                        'role' => Auth::user()->role,
+                    'user' => $request->user() ? [
+                        'id' => $request->user()->id,
+                        'first_name' => $request->user()->first_name,
+                        'last_name' => $request->user()->last_name,
+                        'email' => $request->user()->email,
+                        'role' => $request->user()->role,
                         'account' => [
-                            'id' => Auth::user()->account->id,
-                            'name' => Auth::user()->account->name,
+                            'id' => $request->user()->account->id,
+                            'name' => $request->user()->account->name,
                         ],
                     ] : null,
                 ];
             },
-            'flash' => function () {
+            'flash' => function () use ($request) {
                 return [
-                    'success' => Session::get('success'),
-                    'error' => Session::get('error'),
+                    'success' => $request->session()->get('success'),
+                    'error' => $request->session()->get('error'),
                 ];
             },
         ]);
