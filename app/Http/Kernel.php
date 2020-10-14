4bc1e1f899779db0fb13d0edd64a86fd3fc22b7a
Claudio Dekker <claudiodekker@users.noreply.github.com>
2020-10-14 17:01:07 +0200

    Fix middleware in Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\Inertia::class
+            \App\Http\Middleware\HandleInertiaRequests::class
         ],
 
         'api' => [
