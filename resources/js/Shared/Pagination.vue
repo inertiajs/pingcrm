e1f57c36134a2782c00ae3cdf3f351795f9d3aba
Jonathan Reinink <jonathan@reinink.ca>
2021-02-27 11:34:08 -0500

    Tweak pagination line height

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -2,8 +2,8 @@
   <div v-if="links.length > 3">
     <div class="flex flex-wrap -mb-1">
       <template v-for="(link, key) in links">
-        <div v-if="link.url === null" :key="key" class="mr-1 mb-1 px-4 py-3 text-sm border rounded text-gray-400" v-html="link.label" />
-        <inertia-link v-else :key="key" class="mr-1 mb-1 px-4 py-3 text-sm border rounded hover:bg-white focus:border-indigo-500 focus:text-indigo-500" :class="{ 'bg-white': link.active }" :href="link.url" v-html="link.label" />
+        <div v-if="link.url === null" :key="key" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded" v-html="link.label" />
+        <inertia-link v-else :key="key" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-indigo-500 focus:text-indigo-500" :class="{ 'bg-white': link.active }" :href="link.url" v-html="link.label" />
       </template>
     </div>
   </div>
