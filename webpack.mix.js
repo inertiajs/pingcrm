8c587b82c467f4d9a51185773376dbce539b6982
Vince Kronlein <vkronlein@icloud.com>
2021-03-07 14:58:47 -0700

    added dynamic chunk name setting resolves #112

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,10 @@ const mix = require('laravel-mix')
 const cssImport = require('postcss-import')
 const cssNesting = require('postcss-nesting')
 
+// adding a constant for the @ path since it can't be resolved
+// from aliases during output.
+const componentPath = 'resources/js'
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -24,11 +28,22 @@ mix
     require('tailwindcss'),
   ])
   .webpackConfig({
-    output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
+    output: {
+      chunkFilename: (pathData) => {
+        // first replace / with _ in component path
+        let path = componentPath.replace('/', '_')
+        // next replace this in our pathData ids
+        let name = pathData.chunk.id.replace(path + '_Pages_', '').toLowerCase().replace(/_/g, '-')
+        // set our chunk name to the proper name value
+        pathData.chunk.name = name
+
+        return 'js/[name].js?id=[chunkhash]'
+      }
+    },
     resolve: {
       alias: {
         vue$: 'vue/dist/vue.runtime.esm.js',
-        '@': path.resolve('resources/js'),
+        '@': path.resolve(componentPath),
       },
     },
   })
