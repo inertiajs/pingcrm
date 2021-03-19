f87ce0f644c637103ff4b1810b136d3ebdd4cf9f
Abraham Martinez <mga.abraham@gmail.com>
2021-03-19 12:32:26 -0600

    commit

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\URL;
 use Inertia\Inertia;
 
 class DocumentController extends Controller
