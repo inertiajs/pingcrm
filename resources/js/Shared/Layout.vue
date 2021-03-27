7aa29eaa23a5cf903a926204f7271f901d0457b9
Jonathan Reinink <jonathan@reinink.ca>
2021-03-27 10:19:48 -0400

    Change logout link to a button

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -30,7 +30,7 @@
                 <div class="mt-2 py-2 shadow-xl bg-white rounded text-sm">
                   <inertia-link class="block px-6 py-2 hover:bg-indigo-500 hover:text-white" :href="route('users.edit', user.id)">My Profile</inertia-link>
                   <inertia-link class="block px-6 py-2 hover:bg-indigo-500 hover:text-white" :href="route('users')">Manage Users</inertia-link>
-                  <inertia-link class="block px-6 py-2 hover:bg-indigo-500 hover:text-white" :href="route('logout')" method="post">Logout</inertia-link>
+                  <inertia-link class="block px-6 py-2 hover:bg-indigo-500 hover:text-white w-full text-left" :href="route('logout')" method="post" as="button">Logout</inertia-link>
                 </div>
               </template>
             </dropdown>
