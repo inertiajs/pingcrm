e1492513651ef758bb70fa84448f306fdca5f30e
Bhavuk <71918863+bhavuk-thakral@users.noreply.github.com>
2021-04-20 17:06:52 +0530

    Task edit errorss

--- resources/js/Pages/Tasks/Edit.vue
+++ resources/js/Pages/Tasks/Edit.vue
@@ -16,117 +16,24 @@
           <text-input v-model="form.description" :error="form.errors.description" class="pr-6 pb-8 w-full lg:w-1/2" label="Description" />
           <text-input v-model="form.priority" :error="form.errors.priority" class="pr-6 pb-8 w-full lg:w-1/2" label="Priority" />
           <text-input v-model="form.status" :error="form.errors.status" class="pr-6 pb-8 w-full lg:w-1/2" label="Status" />
-          <text-input v-model="form.deu_date" :error="form.errors.deu_date" class="pr-6 pb-8 w-full lg:w-1/2" label="Deu_date" />
+          <text-input v-model="form.deu_date" :error="form.errors.deu_date" class="pr-6 pb-8 w-full lg:w-1/2" label="Due_date" />
           <text-input v-model="form.completed_date" :error="form.errors.completed_date" class="pr-6 pb-8 w-full lg:w-1/2" label="Completed_date" />
-          <text-input v-model="form.user_id" :error="form.errors.User_id" class="pr-6 pb-8 w-full lg:w-1/2" label="User_id" />
+          <text-input v-model="form.user_id" :error="form.errors.user_id" class="pr-6 pb-8 w-full lg:w-1/2" label="User_id" />
           <text-input v-model="form.task_id" :error="form.errors.task_id" class="pr-6 pb-8 w-full lg:w-1/2" label="Task_id" />
           <text-input v-model="form.project_id" :error="form.errors.project_id" class="pr-6 pb-8 w-full lg:w-1/2" label="Project_id" />
           <text-input v-model="form.team_id" :error="form.errors.team_id" class="pr-6 pb-8 w-full lg:w-1/2" label="Team_id" />
+          
+    
         </div>
         <div class="px-8 py-4 bg-gray-50 border-t border-gray-100 flex items-center">
           <button v-if="!task.deleted_at" class="text-red-600 hover:underline" tabindex="-1" type="button" @click="destroy">Delete task</button>
-          <loading-button :loading="form.processing" class="btn-indigo ml-auto" type="submit">Update task</loading-button>
+          <loading-button :loading="form.processing" class="btn-indigo ml-auto" type="submit">Update Task</loading-button>
         </div>
       </form>
     </div>
-    <h2 class="mt-12 font-bold text-2xl">tasks</h2>
-    <div class="mt-6 bg-white rounded shadow overflow-x-auto">
-      <table class="w-full whitespace-nowrap">
-        <tr class="text-left font-bold">
-          <th class="px-6 pt-6 pb-4">Id</th>
-          <th class="px-6 pt-6 pb-4">Title</th>
-          <th class="px-6 pt-6 pb-4">Description</th>
-          <th class="px-6 pt-6 pb-4">Priority</th>
-          <th class="px-6 pt-6 pb-4">Status</th>
-          <th class="px-6 pt-6 pb-4">Deu_date</th>
-          <th class="px-6 pt-6 pb-4">Completed_date</th>
-          <th class="px-6 pt-6 pb-4">User_id</th>
-          <th class="px-6 pt-6 pb-4">Task_id</th>
-          <th class="px-6 pt-6 pb-4">Project_id</th>
-          <th class="px-6 pt-6 pb-4">Team_id</th>
-          
-        </tr>
-        <tr v-for="task in task.tasks" :key="task.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
-          <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center focus:text-indigo-500" :href="route('tasks.edit',)">
-              
-              <icon v-if="task.deleted_at" name="trash" class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2" />
-            </inertia-link>
-          </td>
 
-          <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-           
-            </inertia-link>
-          </td>
-    
-          <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-              
-            </inertia-link>
-          </td>
-
-           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-              
-            </inertia-link>
-          </td>
-
-           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-             
-            </inertia-link>
-          </td>
-
-           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-              
-            </inertia-link>
-          </td>
-
-           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-              
-            </inertia-link>
-          </td>
-
-           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-              
-            </inertia-link>
-          </td>
-
-           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-        
-            </inertia-link>
-          </td>
-
-           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-              
-            </inertia-link>
-          </td>
-          
-          <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-            </inertia-link>
-          </td>
-
-          <td class="border-t w-px">
-            <inertia-link class="px-4 flex items-center" :href="route('tasks.edit',)" tabindex="-1">
-              <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400" />
-            </inertia-link>
-          </td>
-        </tr>
-        <tr v-if="task.tasks.length === 0">
-          <td class="border-t px-6 py-4" colspan="4">No tasks found.</td>
-        </tr>
-      </table>
-    </div>
   </div>
 </template>
-
 
 <script>
 import Icon from '@/Shared/Icon'
@@ -163,7 +70,7 @@ export default {
         deu_date: this.task.deu_date,
         completed_date: this.task.completed_date,
         user_id: this.task.user_id,
-        task_id: this.task.task,
+        task_id: this.task.task_id,
         project_id: this.task.project_id,
         team_id: this.task.team_id,
       }),
