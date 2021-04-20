744c11c8b9fb093371b630096c19b0782257a012
Lalit J <lalit.jhandai@gmail.com>
2021-04-20 11:22:34 +0530

    Fixed address crud

--- resources/js/Pages/Addresses/Index.vue
+++ resources/js/Pages/Addresses/Index.vue
@@ -10,7 +10,7 @@
           <option value="only">Only Trashed</option>
         </select>
       </search-filter>
-      <inertia-link class="btn-indigo" :href="route('Addresses.create')">
+      <inertia-link class="btn-indigo" :href="route('addresses.create')">
         <span>Create</span>
         <span class="hidden md:inline">Address</span>
       </inertia-link>
@@ -22,35 +22,35 @@
           <th class="px-6 pt-6 pb-4">City</th>
           <th class="px-6 pt-6 pb-4" colspan="2">Phone</th>
         </tr>
-        <tr v-for="address in addresss.data" :key="address.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
+        <tr v-for="address in addresses.data" :key="address.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center focus:text-indigo-500" :href="route('addresses.edit', addresses.id)">
+            <inertia-link class="px-6 py-4 flex items-center focus:text-indigo-500" :href="route('addresses.edit', address.id)">
               {{ address.name }}
               <icon v-if="addresses.deleted_at" name="trash" class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2" />
             </inertia-link>
           </td>
           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('addresss.edit', address.id)" tabindex="-1">
+            <inertia-link class="px-6 py-4 flex items-center" :href="route('addresses.edit', address.id)" tabindex="-1">
               {{ address.city }}
             </inertia-link>
           </td>
           <td class="border-t">
-            <inertia-link class="px-6 py-4 flex items-center" :href="route('addresss.edit', address.id)" tabindex="-1">
+            <inertia-link class="px-6 py-4 flex items-center" :href="route('addresses.edit', address.id)" tabindex="-1">
               {{ address.phone }}
             </inertia-link>
           </td>
           <td class="border-t w-px">
-            <inertia-link class="px-4 flex items-center" :href="route('addresss.edit', address.id)" tabindex="-1">
+            <inertia-link class="px-4 flex items-center" :href="route('addresses.edit', address.id)" tabindex="-1">
               <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400" />
             </inertia-link>
           </td>
         </tr>
-        <tr v-if="addresss.data.length === 0">
-          <td class="border-t px-6 py-4" colspan="4">No addresss found.</td>
+        <tr v-if="addresses.data.length === 0">
+          <td class="border-t px-6 py-4" colspan="4">No addresses found.</td>
         </tr>
       </table>
     </div>
-    <pagination class="mt-6" :links="addresss.links" />
+    <pagination class="mt-6" :links="addresses.links" />
   </div>
 </template>
 
@@ -72,7 +72,7 @@ export default {
   },
   layout: Layout,
   props: {
-    addresss: Object,
+    addresses: Object,
     filters: Object,
   },
   data() {
@@ -87,7 +87,7 @@ export default {
     form: {
       handler: throttle(function() {
         let query = pickBy(this.form)
-        this.$inertia.replace(this.route('addresss', Object.keys(query).length ? query : { remember: 'forget' }))
+        this.$inertia.replace(this.route('addresses', Object.keys(query).length ? query : { remember: 'forget' }))
       }, 150),
       deep: true,
     },
