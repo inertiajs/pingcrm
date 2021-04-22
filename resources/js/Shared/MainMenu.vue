2504e9fb4c51004fc677143127da80c1b9e86f19
Manju Kumari <manjukumari6719@gmail.com>
2021-04-22 15:53:00 +0530

    Users Crud

--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -66,7 +66,13 @@
       </inertia-link>
     </div>
 
-    
+    <div class="mb-4">
+      <inertia-link class="flex items-center group py-3" :href="route('users')">
+        <icon name="users" class="w-4 h-4 mr-2" :class="isUrl('users') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+        <div :class="isUrl('users') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Users</div>
+      </inertia-link>
+    </div>
+
 
     <div class="mb-4">
       <inertia-link class="flex items-center group py-3" :href="route('contacts')">
