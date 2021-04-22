5f53e6b1989ef5233dff0f2de0762a3e804c372b
Manju Kumari <manjukumari6719@gmail.com>
2021-04-22 12:17:24 +0530

    update profile.factory
    Change faker name to company

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Profile::class, function (Faker $faker) {
     return [
-        'name' => $faker->company,
+        'name' => $faker->name,
         'phone' => $faker->tollFreePhoneNumber,
         'city' => $faker->city,
         'job' => $faker->jobTitle,
