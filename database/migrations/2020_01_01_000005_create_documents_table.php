0312d019a6e9248d60c3557395b85b8b1c04c487
Bhavuk <71918863+bhavuk-thakral@users.noreply.github.com>
2021-04-23 16:42:07 +0530

    chnge in table in document

--- database/migrations/2020_01_01_000005_create_documents_table.php
+++ database/migrations/2020_01_01_000005_create_documents_table.php
@@ -22,6 +22,7 @@ class CreateDocumentsTable extends Migration
             $table->string('digit')->nullable();
             $table->string('document_label', 50)->nullable();
             $table->string('length', 50)->nullable();
+           
             $table->timestamps();
             $table->softDeletes();
         });
