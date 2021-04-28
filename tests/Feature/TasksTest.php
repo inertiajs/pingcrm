4de48d56c81bc3f4eff0f804008a321ca7a5779b
Bhavuk <sahilthakral7908@gmail.com>
2021-04-28 14:47:56 +0530

    fixes in task crud

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -40,8 +40,8 @@ class TasksTest extends TestCase
             ->assertPropValue('tasks.data', function ($tasks) {
                 $this->assertEquals(
                     [
-                    'id', 'title', 'description', 'user_id',
-                    'task_id', 'team_id','project_id', 'priority', 'status', 'creator','due_date', 'completed_date', 
+                    'id', 'title', 'description','user_id','task_id', 'team_id','project_id',
+                    'priority', 'status', 'creator','due_date', 'completed_date', 
                     ],
                     array_keys($tasks[0])
                 );
@@ -91,7 +91,7 @@ class TasksTest extends TestCase
             ->assertPropCount('tasks.data', 5);
     }
     
-    public function test_can_view_single_client()
+    public function test_can_view_single_task()
     {
         $task =  $this->user->account->tasks()->saveMany(
             factory(Task::class, 5)->make()
@@ -102,6 +102,7 @@ class TasksTest extends TestCase
         $this->actingAs($this->user)
             ->get('/tasks/' . $task->id . "/edit")
             ->assertStatus(200)
+            ->assertPropValue('task.title', $task->title)
             ->assertPropValue('task.user_id', $task->user_id)
             ->assertPropValue('task.task_id', $task->task_id)
             ->assertPropValue('task.team_id', $task->team_id)
@@ -109,7 +110,8 @@ class TasksTest extends TestCase
             ->assertPropValue('task.priority',$task->priority)
             ->assertPropValue('task.status', $task->status)
             ->assertPropValue('task.creator', $task->creator)
-            ->assertPropValue('task.deu_date', $task->deu_date)
+            ->assertPropValue('task.due_date', $task->due_date)
             ->assertPropValue('task.completed_date', $task->completed_date);
+           ;
     }
 }
