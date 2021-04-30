b8d7e005bf2cad91f372ae8a6a7a7f5717918dcb
Bhavuk <sahilthakral7908@gmail.com>
2021-04-30 14:28:56 +0530

    priority and status changes in task

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -6,8 +6,7 @@ $factory->define(App\Models\Task::class, function (Faker $faker) {
     return [
         'title' => $faker->title,
         'description' => $faker->text,
-        'priority' => 10,
-        'status' => 100,
+
         'user_id' => 1,
         'task_id' => null,
         'project_id' => 1,
