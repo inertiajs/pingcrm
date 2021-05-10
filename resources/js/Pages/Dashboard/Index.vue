ee2716ee4cb6facfd35812b75ada53801f1e1bef
Jonathan Reinink <jonathan@reinink.ca>
2021-05-10 13:05:31 -0400

    Remove "500 error" endpoint that just caused confusion

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -2,11 +2,6 @@
   <div>
     <h1 class="mb-8 font-bold text-3xl">Dashboard</h1>
     <p class="mb-8 leading-normal">Hey there! Welcome to Ping CRM, a demo app designed to help illustrate how <a class="text-indigo-500 underline hover:text-orange-600" href="https://inertiajs.com">Inertia.js</a> works.</p>
-    <div class="mb-8 flex">
-      <inertia-link class="btn-indigo" href="/500">500 error</inertia-link>
-      <inertia-link class="btn-indigo ml-1" href="/404">404 error</inertia-link>
-    </div>
-    <p class="leading-normal">👆 These links are intended to be broken to illustrate how error handling works with Inertia.js.</p>
   </div>
 </template>
 
--- routes/web.php
+++ routes/web.php
@@ -138,14 +138,3 @@ Route::get('reports', [ReportsController::class, 'index'])
 // Images
 
 Route::get('/img/{path}', [ImagesController::class, 'show'])->where('path', '.*');
-
-// 500 error
-
-Route::get('500', function () {
-    // Force debug mode for this endpoint in the demo environment
-    if (App::environment('demo')) {
-        Config::set('app.debug', true);
-    }
-
-    echo $fail;
-});
