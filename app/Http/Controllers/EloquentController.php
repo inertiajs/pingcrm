723c44eefb865110ccc8ec60a689fca2e2538aa1
Bhavuk <sahilthakral7908@gmail.com>
2021-05-27 16:25:01 +0530

    eloquent controller

--- app/Http/Controllers/EloquentController.php
+++ app/Http/Controllers/EloquentController.php
@@ -11,11 +11,23 @@ class EloquentController extends Controller
 {
     public function index()
     {
-        return Task::where('priority',10)->orderBy('description','desc')->pluck('title','status');
-        return Task::where('priority',10)->where('id',5)->first();
-        return Task::where('id',5)->first();
-        return Task::where('id',5)->get();
-        return Task::find(5);
-        return Task::all();
+       return Task::where('id',5)
+                    ->join('status', 'task.id', '=', 'status.team_id')
+                    ->join('priority', 'task.id', '=', 'priority.team_id')
+                    ->select('tasks.*', 'status', 'priority')
+                    ->get();
+       //
+       return Task::where('status',100)->orderBy('title')->chunk(100, function ($tasks) {
+            //foreach ($tasks as $task) {
+                //
+            //}
+        //});
+        
+        //return Task::where('priority',10)->where('id',5)->first();
+       // return Task::where('id',5)->first();
+        //return Task::where('id',5)->get();
+        //return Task::find(5);
+        //return Task::all();
+        //where('priority',10)->orderBy('description','desc')->pluck('task_id','priority')
     }
 }
