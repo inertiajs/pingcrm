3475371de62b53851b77b2b4c95549c65b397c12
Bhavuk <sahilthakral7908@gmail.com>
2021-06-30 12:46:45 +0530

    some changes in model factory and layout files

--- app/Models/RuleCategory.php
+++ app/Models/RuleCategory.php
@@ -16,7 +16,7 @@ class RuleCategory extends Model
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? null, function ($query, $search) {
-            $query->where('title', 'like', '%' . $search . '%');
+            $query->where('name', 'like', '%' . $search . '%');
         })->when($filters['trashed'] ?? null, function ($query, $trashed) {
             if ($trashed === 'with') {
                 $query->withTrashed();
--- database/factories/RuleCategoryFactory.php
+++ database/factories/RuleCategoryFactory.php
@@ -6,9 +6,9 @@ $factory->define(App\Models\RuleCategory::class, function (Faker $faker) {
     return [
         'name' => $faker->company,
         'description' => $faker->text,
-        'category' => null,
-        'parent_id' => null,
-        'instructor_id' => null,
+        'category' => 1,
+        'parent_id' => 12,
+        'instructor_id' => 23,
         'priority' =>100,
     ];
 });
