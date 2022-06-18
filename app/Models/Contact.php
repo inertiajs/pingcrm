3bafd873fd5cc04263b1c0e4379e9bb0596e3ef4
mchev <martin.chevignard@gmail.com>
2022-06-18 18:53:14 +0200

    Add the missing scope orderByName

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -26,6 +26,11 @@ class Contact extends Model
         return $this->first_name.' '.$this->last_name;
     }
 
+    public function scopeOrderByName($query)
+    {
+        $query->orderBy('last_name')->orderBy('first_name');
+    }
+    
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? null, function ($query, $search) {
