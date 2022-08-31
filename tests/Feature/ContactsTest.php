2ced71e0a61c50e6982ab5c8a4fcf180b942a719
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-08-31 16:41:58 +0300

    Use class alias instead

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use App\Models\Account;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Inertia\Testing\AssertableInertia;
+use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
 class ContactsTest extends TestCase
@@ -57,26 +57,26 @@ class ContactsTest extends TestCase
     {
         $this->actingAs($this->user)
             ->get('/contacts')
-            ->assertInertia(fn (AssertableInertia $assert) => $assert
+            ->assertInertia(fn (Assert $assert) => $assert
                 ->component('Contacts/Index')
                 ->has('contacts.data', 2)
-                ->has('contacts.data.0', fn (AssertableInertia $assert) => $assert
+                ->has('contacts.data.0', fn (Assert $assert) => $assert
                     ->where('id', 1)
                     ->where('name', 'Martin Abbott')
                     ->where('phone', '555-111-2222')
                     ->where('city', 'Murphyland')
                     ->where('deleted_at', null)
-                    ->has('organization', fn (AssertableInertia $assert) => $assert
+                    ->has('organization', fn (Assert $assert) => $assert
                         ->where('name', 'Example Organization Inc.')
                     )
                 )
-                ->has('contacts.data.1', fn (AssertableInertia $assert) => $assert
+                ->has('contacts.data.1', fn (Assert $assert) => $assert
                     ->where('id', 2)
                     ->where('name', 'Lynn Kub')
                     ->where('phone', '555-333-4444')
                     ->where('city', 'Woodstock')
                     ->where('deleted_at', null)
-                    ->has('organization', fn (AssertableInertia $assert) => $assert
+                    ->has('organization', fn (Assert $assert) => $assert
                         ->where('name', 'Example Organization Inc.')
                     )
                 )
@@ -87,17 +87,17 @@ class ContactsTest extends TestCase
     {
         $this->actingAs($this->user)
             ->get('/contacts?search=Martin')
-            ->assertInertia(fn (AssertableInertia $assert) => $assert
+            ->assertInertia(fn (Assert $assert) => $assert
                 ->component('Contacts/Index')
                 ->where('filters.search', 'Martin')
                 ->has('contacts.data', 1)
-                ->has('contacts.data.0', fn (AssertableInertia $assert) => $assert
+                ->has('contacts.data.0', fn (Assert $assert) => $assert
                     ->where('id', 1)
                     ->where('name', 'Martin Abbott')
                     ->where('phone', '555-111-2222')
                     ->where('city', 'Murphyland')
                     ->where('deleted_at', null)
-                    ->has('organization', fn (AssertableInertia $assert) => $assert
+                    ->has('organization', fn (Assert $assert) => $assert
                         ->where('name', 'Example Organization Inc.')
                     )
                 )
@@ -110,7 +110,7 @@ class ContactsTest extends TestCase
 
         $this->actingAs($this->user)
             ->get('/contacts')
-            ->assertInertia(fn (AssertableInertia $assert) => $assert
+            ->assertInertia(fn (Assert $assert) => $assert
                 ->component('Contacts/Index')
                 ->has('contacts.data', 1)
                 ->where('contacts.data.0.name', 'Lynn Kub')
@@ -123,7 +123,7 @@ class ContactsTest extends TestCase
 
         $this->actingAs($this->user)
             ->get('/contacts?trashed=with')
-            ->assertInertia(fn (AssertableInertia $assert) => $assert
+            ->assertInertia(fn (Assert $assert) => $assert
                 ->component('Contacts/Index')
                 ->has('contacts.data', 2)
                 ->where('contacts.data.0.name', 'Martin Abbott')
