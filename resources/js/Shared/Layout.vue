331ab824d3e8e5ccacf8367151cfed76b10cfedd
shuvo-me <shuvo.js.dev@gmail.com>
2023-12-10 16:45:33 +0600

    removed: commented code

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -53,29 +53,6 @@
   </div>
 </template>
 
-<!-- <script>
-import { Link } from '@inertiajs/inertia-vue3'
-import Icon from '@/Shared/Icon'
-import Logo from '@/Shared/Logo'
-import Dropdown from '@/Shared/Dropdown'
-import MainMenu from '@/Shared/MainMenu'
-import FlashMessages from '@/Shared/FlashMessages'
-
-export default {
-  components: {
-    Dropdown,
-    FlashMessages,
-    Icon,
-    Link,
-    Logo,
-    MainMenu,
-  },
-  props: {
-    auth: Object,
-  },
-}
-</script> -->
-
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3'
 import Icon from '@/Shared/Icon'
