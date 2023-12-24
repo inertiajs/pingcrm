a9839acc49981f8d2c482d65b45698875ad3d5b4
shuvo-me <shuvo.js.dev@gmail.com>
2023-12-24 09:56:24 +0600

    updated: Dropdown component from option api to composition api

--- resources/js/Shared/Dropdown.vue
+++ resources/js/Shared/Dropdown.vue
@@ -1,10 +1,10 @@
 <template>
-  <button type="button" @click="show = true">
+  <button ref="$el" type="button" @click="show = true">
     <slot />
     <teleport v-if="show" to="#dropdown">
       <div>
         <div style="position: fixed; top: 0; right: 0; left: 0; bottom: 0; z-index: 99998; background: black; opacity: 0.2" @click="show = false" />
-        <div ref="dropdown" style="position: absolute; z-index: 99999" @click.stop="show = !autoClose">
+        <div ref="dropdownRef" style="position: absolute; z-index: 99999" @click.stop="show = !autoClose">
           <slot name="dropdown" />
         </div>
       </div>
@@ -12,52 +12,51 @@
   </button>
 </template>
 
-<script>
+<script setup>
 import { createPopper } from '@popperjs/core'
+import { ref, nextTick, watch, onMounted } from 'vue'
 
-export default {
-  props: {
-    placement: {
-      type: String,
-      default: 'bottom-end',
-    },
-    autoClose: {
-      type: Boolean,
-      default: true,
-    },
+const props = defineProps({
+  placement: {
+    type: String,
+    default: 'bottom-end',
   },
-  data() {
-    return {
-      show: false,
+  autoClose: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+const show = ref(false)
+const popper = ref(null)
+const $el = ref(null)
+const dropdownRef = ref(null)
+
+watch(show, () => {
+  nextTick(() => {
+    if (show.value === true) {
+      popper.value = createPopper($el.value, dropdownRef.value, {
+        placement: props.placement,
+        modifiers: [
+          {
+            name: 'preventOverflow',
+            options: {
+              altBoundary: true,
+            },
+          },
+        ],
+      })
+    } else if (popper.value) {
+      setTimeout(() => popper.value.destroy(), 100)
     }
-  },
-  watch: {
-    show(show) {
-      if (show) {
-        this.$nextTick(() => {
-          this.popper = createPopper(this.$el, this.$refs.dropdown, {
-            placement: this.placement,
-            modifiers: [
-              {
-                name: 'preventOverflow',
-                options: {
-                  altBoundary: true,
-                },
-              },
-            ],
-          })
-        })
-      } else if (this.popper) {
-        setTimeout(() => this.popper.destroy(), 100)
-      }
-    },
-  },
-  mounted() {
-    document.addEventListener('keydown', (e) => {
-      if (e.key === 'Escape') {
-        this.show = false
-      }
-    })
-  },
-}
+  })
+})
+
+onMounted(() => {
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      show.value = false
+    }
+  })
+})
 </script>
