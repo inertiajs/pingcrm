df69579a0e7f855cf730f20a562f9314ea9a0bc0
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2024-08-07 13:32:21 +0600

    Add Migration for Invoice

---
+++ app/Models/Invoice.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+    protected $table = 'invoices';
+    protected $fillable = ['number', 'amount', 'organization_id', 'contact_id'];
+}
---
+++ database/migrations/2024_08_07_071222_create_invoices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->decimal('amount', 15, 2);
+            $table->integer('organization_id');
+            $table->integer('contact_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
