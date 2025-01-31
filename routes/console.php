cd187f887d8dd86a2d8c760c8511b2e74f719cd1
André Valentin <andre@laravel.com>
2025-01-31 22:53:05 +0100

    Add scheduled commands to reset db with fresh data

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Reset demo environment daily at midnight
+Schedule::command('migrate:fresh')->dailyAt('00:00');
+Schedule::command('db:seed')->dailyAt('00:00');
